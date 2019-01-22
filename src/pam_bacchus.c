3760f1e54e1e326fc08cd73b2d9c10b7cf788b1e
skystar-p <usezmap@gmail.com>
2019-01-23 02:10:37 +0900

    cleanup redundant code

--- src/pam_bacchus.c
+++ src/pam_bacchus.c
@@ -8,28 +8,8 @@
 
 #define ID_LOGIN_ENDPOINT "https://id.snucse.org/api/login"
 
-struct response_data {
-    char *data;
-    size_t size;
-};
-
 static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userdata) {
-    size_t realsize = size * nmemb;
-    /*
-    struct response_data *current_data = (struct response_data *) userdata;
-
-    char *reallocated_ptr = (char *) realloc(current_data->data, current_data->size + realsize + 1);
-    if (reallocated_ptr == NULL) {
-        // error, not enough memory
-        return 0;
-    }
-
-    current_data->data = reallocated_ptr;
-    memcpy(&(current_data->data[current_data->size]), contents, realsize);
-    current_data->size += realsize;
-    current_data->data[current_data->size] = '\0';
-    */
-    return realsize;
+    return size * nmemb;
 }
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
@@ -92,13 +72,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
         return PAM_AUTH_ERR;
     }
 
-    /*
-    struct response_data userdata;
-    // preallocate for a null character
-    userdata.data = (char *) malloc(1);
-    userdata.size = 0;
-    */
-
     curl_easy_setopt(curl, CURLOPT_URL, URL);
 
     headers = curl_slist_append(headers, "Accept: applicaton/json");
@@ -113,7 +86,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
 
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_body);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    // curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &userdata);
 
     // synchronous http call
     curl_code = curl_easy_perform(curl);
@@ -130,7 +102,6 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
         }
     }
 
-    // free(userdata.data);
     if (pam_conv_data) {
         memset(pam_resp[0].resp, '\0', strlen(pam_resp[0].resp));
         free(pam_resp);
